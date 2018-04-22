0a457f3b7b075109875a09fc69958223a19f2b95
praveen t n <tnpraveen87@gmail.com>
2018-04-22 21:01:55 +0530

    Create teabull.py

---
+++ samples/average_returns_from_stocks/teabull.py
@@ -0,0 +1,7 @@
+# PROBLEM
+# A stock market analyst wants to estimate the average return 
+# on a certain stock. A random sample of 15 days yields an average 
+# (annualized) return of Xbar=10.37% and a standard deviation of s=3.5%. 
+# Assuming a normal population of returns, give a 95% confidence 
+# interval for the average return on this stock.
+
